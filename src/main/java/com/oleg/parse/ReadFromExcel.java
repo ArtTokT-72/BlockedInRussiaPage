c0f883ca437291ef0f884ed67e4b4c450de86c81
otkachenko <otkachenko@wiley.com>
2018-07-23 17:36:54 +0300

    updated some methods

---
+++ src/main/java/com/oleg/parse/ReadFromExcel.java
@@ -0,0 +1,70 @@
+package com.oleg.parse;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * class allows you to do the parsing of the file type xls.
+ * The data is placed in the Arraylist collection
+ */
+
+public class ReadFromExcel {
+
+    public List<String> read(String fileName) {
+
+        InputStream in;
+        List<String> result = new ArrayList<String>();
+        HSSFWorkbook wb = null;
+        wb = getSheets(wb);
+
+        if (wb != null) {
+            Sheet sheet = wb.getSheetAt(0);
+
+            int rowNumber = sheet.getLastRowNum() + 1;
+            for (int i = 0; i < rowNumber; i++) {
+
+                Row row = sheet.getRow(i);
+
+                Cell cell = row.getCell(0);
+                int cellType = cell.getCellType();
+
+                switch (cellType) {
+                    case Cell.CELL_TYPE_STRING:
+                        result.add(cell.getStringCellValue());
+                        break;
+                    case Cell.CELL_TYPE_NUMERIC:
+                        result.add(Double.toString(cell.getNumericCellValue()));
+                        break;
+
+                    case Cell.CELL_TYPE_FORMULA:
+                        result.add(Double.toString(cell.getNumericCellValue()));
+                        break;
+                    default:
+                        result.add("|");
+                        break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private HSSFWorkbook getSheets(HSSFWorkbook wb) {
+        InputStream in;
+        try {
+            in = new FileInputStream("E:/test/test.xls");
+            wb = new HSSFWorkbook(in);
+        } catch (IOException e) {
+        }
+        return wb;
+    }
+}
+
--- src/test/java/com/oleg/blockedtest/BlockedTest.java
+++ src/test/java/com/oleg/blockedtest/BlockedTest.java
@@ -1,8 +1,7 @@
 package com.oleg.blockedtest;
 
 import com.oleg.page.BlockedInRussiaPage;
-import com.oleg.parse.ExcelUtil;
-import com.oleg.parse.Parser;
+import com.oleg.parse.ReadFromExcel;
 import com.wiley.basetests.SeleniumBaseTest;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
@@ -14,75 +13,54 @@ import org.testng.annotations.Test;
 import java.io.*;
 import java.util.Date;
 
-import static java.lang.Thread.sleep;
-
 /**
  * Finds the necessary locator and inserts a text value into it.
  */
 
 public class BlockedTest extends SeleniumBaseTest {
 
-    @Test
-    public void testCheckUrls() throws Exception {
+    static final String FILE_NAME_AND_DIRECTORY = "E:/test/test.xls";
 
-        Parser parser = new Parser();
+    @Test
+    public void testCheckUrls() {
+
+        ReadFromExcel readFromExcel = new ReadFromExcel();
         BlockedInRussiaPage page = get(BlockedInRussiaPage.class, "http://isitblockedinrussia.com");
 
-        for (int i = 0; i < parser.parse().size(); i++) {
-            String url = parser.parse().get(i);
+        for (int i = 0; i < readFromExcel.read(FILE_NAME_AND_DIRECTORY).size(); i++) {
+            String url = readFromExcel.read(FILE_NAME_AND_DIRECTORY).get(i);
             page.inputUrl(url);
             BlockedInRussiaPage blockedInRussiaPage = new BlockedInRussiaPage();
 
             if (blockedInRussiaPage.checkAdressIsBlockt()) {
-                String fileName = "E:/test/test.xls";
 
                 FileInputStream inputStream = null;
-                try {
-                    inputStream = new FileInputStream(new File("E:/test/test.xls"));
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
+                inputStream = getFileInputStream(inputStream);
                 Workbook book = null;
-                try {
-                    assert inputStream != null;
-                    book = new HSSFWorkbook(inputStream);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                assert book != null;
-                Sheet sheet = book.getSheet("Sheet0");
-                Row row = sheet.getRow(i);
-                Cell status = row.createCell(1);
-                status.setCellValue("address available");
+                book = getSheets((FileInputStream) inputStream, (Workbook) book);
 
-                Cell data = row.createCell(2);
-                Date date = new Date();
-                data.setCellValue(date.toString());
+                if (book != null) {
+                    Sheet sheet = book.getSheet("Sheet0");
 
-                try {
-                    book.write(new FileOutputStream("E:/test/test.xls"));
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    Row row = sheet.getRow(i);
+                    Cell status = row.createCell(1);
+                    status.setCellValue("address available");
+
+                    Cell data = row.createCell(2);
+                    Date date = new Date();
+                    data.setCellValue(date.toString());
+                    getWorkbook(book);
                 }
 
             } else {
+
                 FileInputStream inputStream = null;
-                try {
-                    inputStream = new FileInputStream(new File("E:/test/test.xls"));
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
+                inputStream = getFileInputStream(inputStream);
 
                 Workbook book = null;
-                try {
-                    assert inputStream != null;
-                    book = new HSSFWorkbook(inputStream);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                book = getSheets(inputStream, book);
                 assert book != null;
                 Sheet sheet = book.getSheet("Sheet0");
-
                 Row row = sheet.getRow(i);
 
                 Cell status = row.createCell(1);
@@ -91,13 +69,35 @@ public class BlockedTest extends SeleniumBaseTest {
                 Cell data = row.createCell(2);
                 Date date = new Date();
                 data.setCellValue(date.toString());
-                try {
-                    book.write(new FileOutputStream("E:/test/test.xls"));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                getWorkbook(book);
 
             }
         }
+    }
+
+    private Workbook getSheets(FileInputStream inputStream, Workbook book) {
+        try {
+            if (inputStream != null)
+                book = new HSSFWorkbook(inputStream);
+        } catch (IOException e) {
+        }
+        return book;
+    }
+
+    private void getWorkbook(Workbook book) {
+        try {
+            book.write(new FileOutputStream(FILE_NAME_AND_DIRECTORY));
+        } catch (IOException e) {
+
+        }
+    }
+
+    private FileInputStream getFileInputStream(FileInputStream inputStream) {
+        try {
+            inputStream = new FileInputStream(new File(FILE_NAME_AND_DIRECTORY));
+        } catch (FileNotFoundException e) {
+
+        }
+        return inputStream;
     }
 }
